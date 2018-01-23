5dec462b5f8d20c1ec09b66e4acccce387ca3316
Eiso Kant <contact@eisokant.com>
2018-01-23 08:42:01 +0100

    adds initial functions for UAST parsing of blobs with bblfsh
    Signed-off-by: Eiso Kant <contact@eisokant.com>

--- examples/git/git_example.go
+++ examples/git/git_example.go
@@ -9,11 +9,15 @@ import (
 	"github.com/chrislusf/gleam/gio"
 	"github.com/chrislusf/gleam/plugins/git"
 	"github.com/chrislusf/gleam/util"
+	"gopkg.in/bblfsh/client-go.v2"
+	protocol "gopkg.in/bblfsh/sdk.v1/protocol"
 )
 
 var (
 	isDistributed   = flag.Bool("distributed", false, "run in distributed or not")
 	isDockerCluster = flag.Bool("onDocker", false, "run in docker cluster")
+
+	registeredUAST = gio.RegisterMapper(uast)
 )
 
 func main() {
@@ -23,7 +27,7 @@ func main() {
 
 	f := flow.New("Git pipeline")
 
-	path := "/home/mthek/engine/**"
+	path := "/home/mthek/engine-small/**"
 
 	repos := f.Read(git.Repositories(path, 1))
 
@@ -34,7 +38,9 @@ func main() {
 
 	join1 := repos.JoinByKey("Repos & Refs", refs)
 
-	join := join1.JoinByKey("Repos & Refs & Blobs", blobs).
+	join := join1.JoinByKey("Repos & Refs & Blobs", blobs)
+
+	q := join.Map("uast", registeredUAST).
 		OutputRow(func(row *util.Row) error {
 			repositoryID := gio.ToString(row.K[0])
 			repositoryURLs := row.V[0]
@@ -42,22 +48,24 @@ func main() {
 			refName := gio.ToString(row.V[2])
 			blobHash := gio.ToString(row.V[3])
 			blobContent := truncateString(gio.ToString(row.V[4]), 20)
-			fmt.Printf("%s : %s : %s : %s : %s : %s\n",
+			language := gio.ToString(row.V[5])
+			fmt.Printf("%s : %s : %s : %s : %s : %s : %s\n",
 				repositoryID,
 				repositoryURLs,
 				refHash,
 				refName,
 				blobHash,
-				blobContent)
+				blobContent,
+				language)
 			return nil
 		})
 
 	if *isDistributed {
-		join.Run(distributed.Option())
+		q.Run(distributed.Option())
 	} else if *isDockerCluster {
-		join.Run(distributed.Option().SetMaster("master:45326"))
+		q.Run(distributed.Option().SetMaster("master:45326"))
 	} else {
-		join.Run()
+		q.Run()
 	}
 }
 
@@ -70,4 +78,28 @@ func truncateString(str string, num int) string {
 		b = str[0:num] + "..."
 	}
 	return b
+}
+
+func uast(x []interface{}) error {
+
+	client, err := bblfsh.NewClient("0.0.0.0:9432")
+	if err != nil {
+		panic(err)
+	}
+
+	blob := gio.ToString(x[4])
+
+	// TODO language classification with Enry as an earlier step
+	res, err := client.NewParseRequest().Language("python").Content(blob).Do()
+	if err != nil {
+		panic(err)
+	}
+
+	if res.Response.Status == protocol.Fatal {
+		res.Language = ""
+	}
+
+	gio.Emit(x[0], x[1], x[2], x[3], x[4], x[5], res.Language)
+
+	return nil
 }
