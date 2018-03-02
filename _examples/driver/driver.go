fd26e1dd0d1972df8e04f46e94fca1b9448bbef8
Eiso Kant <contact@eisokant.com>
2018-03-02 10:45:10 +0000

    minor

--- _examples/driver/driver.go
+++ _examples/driver/driver.go
@@ -69,7 +69,7 @@ func queryExample(path, query string) (*flow.Dataset, []flow.FlowOption, error) 
 
 	switch query {
 	case "test":
-		p = f.Read(engine.Repositories(path, 2))
+		p = f.Read(engine.Repositories(path, 2).References().Commits())
 	default:
 		return nil, nil, errors.New("this query is not implemented")
 	}
